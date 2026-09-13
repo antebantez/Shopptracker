9daba62acb897ce51f38a3fbd0491585cfed7ade
Anton Olsson <antonolsson.imp@gmail.com>
2026-09-13 20:17:57 +0200

    Add backend base

---
+++ backend/src/main/java/com/antebantez/shopptracker/ShopptrackerApplication.java
@@ -0,0 +1,13 @@
+package com.antebantez.shopptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopptrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopptrackerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/antebantez/shopptracker/ShopptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antebantez.shopptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopptrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
