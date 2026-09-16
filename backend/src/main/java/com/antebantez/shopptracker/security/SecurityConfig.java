f6d993d705004bced8c9fd98a982574dceb6b12f
Anton Olsson <antonolsson.imp@gmail.com>
2026-09-16 23:55:43 +0200

    permitAll on login endpoint

--- backend/src/main/java/com/antebantez/shopptracker/security/SecurityConfig.java
+++ backend/src/main/java/com/antebantez/shopptracker/security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/register").permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 );
 
